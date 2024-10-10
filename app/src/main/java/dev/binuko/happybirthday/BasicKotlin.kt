b7e9d4a3bb810480427cc6361a340dc459aa872e
Angger Binuko Paksi <angger.binuko@gmail.com>
2024-10-10 14:45:54 +0700

    [add] - praktik dasar kotlin pertemuan 6

---
+++ app/src/main/java/dev/binuko/happybirthday/BasicKotlin.kt
@@ -0,0 +1,13 @@
+package dev.binuko.happybirthday
+
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    // Fill in the code.
+}
